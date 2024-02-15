c6787f7cffd8fbe930cf65f767ce3793d3d720d2
Rjynth <149914497+Rjynth@users.noreply.github.com>
2024-02-15 17:31:41 +0300

    Add files via upload

---
+++ hw.sql
@@ -0,0 +1,50 @@
+
+CREATE DATABASE MusicDB;
+
+
+USE MusicDB;
+
+
+CREATE TABLE Genres (
+  GenreID INT PRIMARY KEY,
+  Genre_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Artists (
+  ArtistID INT PRIMARY KEY,
+  Artist_name VARCHAR(50) NOT NULL,
+  Artist_alias VARCHAR(50)
+);
+
+CREATE TABLE genre_band (
+  genreID INT,
+  artistID INT,
+  PRIMARY KEY (genreID, artistID),
+  FOREIGN KEY (genreID) REFERENCES Genres(GenreID),
+  FOREIGN KEY (artistID) REFERENCES Artists(ArtistID)
+);
+
+CREATE TABLE Albums (
+  AlbumID INT PRIMARY KEY,
+  Albums_name VARCHAR(50) NOT NULL,
+  Albums_year INT
+);
+
+CREATE TABLE Tracks (
+  TrackID INT PRIMARY KEY,
+  Track_name VARCHAR(50) NOT NULL,
+  Track_time INT,
+  AlbumID INT,
+  FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID)
+);
+
+CREATE TABLE track_collection (
+  track_collectionID INT PRIMARY KEY,
+  collectionID INT,
+  FOREIGN KEY (collectionID) REFERENCES collection(collectionID)
+);
+
+CREATE TABLE collection (
+  collectionID INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
