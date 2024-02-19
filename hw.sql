8f881c18659ff17474595bc9efc63aa1c4ff2cb2
Rjynth <149914497+Rjynth@users.noreply.github.com>
2024-02-19 22:09:27 +0300

    Update hw.sql

--- hw.sql
+++ hw.sql
@@ -10,33 +10,38 @@ CREATE TABLE Genres (
   Genre_name VARCHAR(50) NOT NULL
 );
 
+
 CREATE TABLE Artists (
   ArtistID INT PRIMARY KEY,
   Artist_name VARCHAR(50) NOT NULL,
   Artist_alias VARCHAR(50)
 );
 
-CREATE TABLE genre_band (
-  genreID INT,
-  artistID INT,
-  PRIMARY KEY (genreID, artistID),
-  FOREIGN KEY (genreID) REFERENCES Genres(GenreID),
-  FOREIGN KEY (artistID) REFERENCES Artists(ArtistID)
+
+CREATE TABLE album_artists (
+  AlbumID INT,
+  ArtistID INT,
+  PRIMARY KEY (AlbumID, ArtistID),
+  FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID),
+  FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID)
 );
+
 
 CREATE TABLE Albums (
   AlbumID INT PRIMARY KEY,
   Albums_name VARCHAR(50) NOT NULL,
-  Albums_year INT
+  Albums_year INT CHECK (Albums_year >= 2000)
 );
+
 
 CREATE TABLE Tracks (
   TrackID INT PRIMARY KEY,
   Track_name VARCHAR(50) NOT NULL,
-  Track_time INT,
+  Track_time INT CHECK (Track_time >= 30),
   AlbumID INT,
   FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID)
 );
+
 
 CREATE TABLE track_collection (
   track_collectionID INT PRIMARY KEY,
@@ -44,7 +49,9 @@ CREATE TABLE track_collection (
   FOREIGN KEY (collectionID) REFERENCES collection(collectionID)
 );
 
+
 CREATE TABLE collection (
   collectionID INT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL
+  name VARCHAR(50) NOT NULL,
+  date_release DATE
 );
